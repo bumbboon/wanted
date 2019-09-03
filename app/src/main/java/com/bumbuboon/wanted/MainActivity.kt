b0b6c789ae2e215a771b7d83a8b76f8b41624fb5
bumbboon <bumbuboon@gmail.com>
2019-09-03 22:51:48 +0900

    [add] first commit

---
+++ app/src/main/java/com/bumbuboon/wanted/MainActivity.kt
@@ -0,0 +1,94 @@
+package com.bumbuboon.wanted
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.EditText
+import android.widget.Toast
+import android.widget.Button
+
+import kotlinx.android.synthetic.main.activity_main.*
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        auth = FirebaseAuth.getInstance()
+
+        val buttonSignUp = findViewById<Button>(R.id.SignUpButton)
+        val buttonLogin = findViewById<Button>(R.id.LoginButton)
+
+        buttonSignUp.setOnClickListener {
+            val emailEditText = findViewById<EditText>(R.id.emailEditText)
+            val emailText = emailEditText.text.toString()
+            val passEditText = findViewById<EditText>(R.id.passEditText)
+            val passText = passEditText.text.toString()
+            auth.createUserWithEmailAndPassword(emailText, passText)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            baseContext, "SignUp 成功",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        Toast.makeText(
+                            baseContext, "SignUp 失敗",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
+
+        buttonLogin.setOnClickListener {
+            val emailEditText = findViewById<EditText>(R.id.emailEditText)
+            val emailText = emailEditText.text.toString()
+            val passEditText = findViewById<EditText>(R.id.passEditText)
+            val passText = passEditText.text.toString()
+
+            auth.signInWithEmailAndPassword(emailText, passText)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            baseContext, "Login 成功",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        Toast.makeText(
+                            baseContext, "Login 失敗",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
+
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
